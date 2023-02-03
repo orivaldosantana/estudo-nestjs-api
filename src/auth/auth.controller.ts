559fd5750a74a7970348a6a48b1ad228fe2b80b3
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-02-03 14:11:46 -0300

    validação inicial dos dados das requisições

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, Get } from "@nestjs/common";
+import { Controller, Post, Get, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) { 
+    console.log({dto, })  
     return this.authService.signup() 
   }
 
