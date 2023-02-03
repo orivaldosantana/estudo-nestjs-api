dbf3ecc53949869cdaf717deafa7b5fd7e06af9d
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-02-02 22:44:10 -0300

    Estrutura inicial do módulo "prisma"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
  
  
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, BookmarkModule , PrismaModule],
  
 })
 export class AppModule {}
