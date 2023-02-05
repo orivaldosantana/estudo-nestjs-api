f08f403800526376dbb40baf3dc62bfab728c742
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-02-05 19:40:53 -0300

    recuperando info do usuário com um token válido

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'passport-strategy/node_modules/@types/express';
 
 @Controller('users')
 export class UserController {
   @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe(){
-    return 'user info'
+  getMe(@Req() req: Request){
+    return req.user 
   }
 
 }
